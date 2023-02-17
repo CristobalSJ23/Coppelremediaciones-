f729bd5ce56f7725513904f1d25ee8bc5e008ac7
oscar vega <oscar_rvr@outlook.com>
2023-02-16 21:30:02 -0600

    ORVR: Correccion del login

--- models/loginModel.php
+++ models/loginModel.php
@@ -9,14 +9,24 @@ class LoginModel{
     }
 
     public function validate($datos){
-       $query = "select * from personas where mail_per = '".$datos['usuario']."' and pas_per ='".$datos['pass']."'";
+       $query = "select usu.correo as correo,
+                usu.nombre as nombre,
+                usu.apt_pat as apt_pat,
+                usu.apt_mat as apt_mat ,
+                r_usu.id_rol as rol,
+                usu.id_usuario as id_usuario
+                from co_usuarios usu
+       INNER JOIN co_r_usu r_usu ON r_usu.id_usuario = usu.id_usuario 
+       where usu.correo = '".$datos['usuario']."' 
+       AND usu.password ='".$datos['pass']."' AND usu.estatus = 1";
+       
        $res=mysqli_query($this->con,$query);
        if(mysqli_num_rows($res)>0){
             while($row = mysqli_fetch_assoc($res)){
-                $data['mail'] = $row['mail_per'];
-                $data['nombre'] = $row['nombre_per'];
-                $data['rol'] = $row['fk_id_rol'];
-                $data['id'] = $row['id_per'];
+                $data['mail'] = $row['correo'];
+                $data['nombre'] = $row['nombre'].' '.$row['apt_pat'].' '.$row['apt_mat'];
+                $data['rol'] = $row['rol'];
+                $data['id'] = $row['id_usuario'];
 
             }
        }else{
