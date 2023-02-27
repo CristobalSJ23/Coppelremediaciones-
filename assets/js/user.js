32a750b2ce5dd579fd1f4398183e6fb9da3f131e
Manuel De La Cruz <manuel.delacruz@softtek.com>
2023-02-27 12:47:53 -0600

    MCV: error usuario

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,10 +1,63 @@
 $(document).ready(function(){
+   
 $('.editar').click(function(){
+    var activo = '',
+    inactivo = '';
+   
+  
     var id = $(this).data('id');
     var htmlNombre = $('.editar_nombre_'+id).html();
     var htmlPaterno = $('.apt_pat'+id).html();
     var htmlMat = $('.apt_mat'+id).html();
-    var htmlNombre = $('.editar_nombre_'+id).html();
+    var htmltipoUsuario = $('.tipo_usuario'+id).html();
+    var htmlCorreo = $('.correo'+id).html();
+    var htmlfechaReg = $('.fecha_reg'+id).html();
+    var htmleditarEstatus = $('.editar_estatus_'+id).html();
+
+    var color = $('.editar_estatus_' + id).data('color');
+
+    $('.editar_nombre_'+id).html('<input name="editarNombre' + id + '" class="editarNombre'+ id + '" value="' + htmlNombre + '"/>');
+    $('.apt_pat'+id).html('<input name="aptPat' + id + '" class="aptPat'+ id + '" value="' + htmlPaterno + '"/>');
+    $('.apt_mat'+id).html('<input name="aptMat' + id + '" class="aptMat'+ id + '" value="' + htmlMat + '"/>');
+    $('.tipo_usuario'+id).html('<input name="tipoUsuario' + id + '" class="tipoUsuario'+ id + '" value="' + htmltipoUsuario + '"/>');
+    $('.correo'+id).html('<input name="correo' + id + '" class="correo'+ id + '" value="' + htmlCorreo + '"/>');
+    $('.editar_estatus_'+id).html('<input name="editarEstatus' + id + '" class="editarEstatus'+ id + '" value="' + htmleditarEstatus + '"/>');
+   
+    $('.editar_estatus_' + id).removeClass(color);
+
+    if (htmleditarEstatus == 'ACTIVO') {
+        activo = 'selected';
+    } else {
+        inactivo = 'selected';
+    }
+    $('.editar_estatus_' + id).html('<select name="guardar_estatus_' + id + '" class="guardar_estatus_' + id + '"> <option value="1" ' + activo + '>ACTIVO</option><option value="0" ' + inactivo + '>INACTIVO</option> </select>');
+
+    $('.editar_acciones_' + id).hide();
+    $('.editar_acciones_cancelar' + id).removeAttr('style');
+    
+});
+
+$('.cancelar').click(function() {
+    var id = $(this).data('id');
+    var htmlNombre = $(this).data('nombre');
+    var htmlPaterno = $(this).data('paterno');
+    var htmlMat = $(this).data('materno');
+    var htmltipoUsuario = $(this).data('tipoUsuario');
+    alert(htmltipoUsuario);
+    var htmlCorreo = $(this).data('correo');
+    var htmleditarEstatus = $(this).data('estatus');
+    var color = $(this).data('color');
+
+    $('.editar_nombre_' + id).html(htmlNombre);
+    $('.apt_pat' + id).html(htmlPaterno);
+    $('.apt_mat' + id).html(htmlMat);
+    $('.tipo_usuario' + id).html(htmltipoUsuario);
+    $('.tipo_usuario1').html("Hola");
+    $('.correo' + id).html(htmlCorreo);
+    $('.editar_estatus_' + id).html(htmleditarEstatus);
+    $('.editar_estatus_' + id).addClass(color);
+    $('.editar_acciones_' + id).show();
+    $('.editar_acciones_cancelar' + id).hide();
 });
 
 });
--- views/user.php
+++ views/user.php
@@ -23,11 +23,11 @@ foreach($res['id_usuario'] as $i => $r){
 <tr>
       <th scope="row"> <?php echo $i; ?> </th>
       <td class="editar_nombre_<?= $r ?>" data-id="<?=$r?>"><?php echo $res['nombre'][$i] ?></td>
-      <td><?php echo $res['apt_pat'][$i]; ?></td>
-      <td><?php echo $res['apt_mat'][$i]; ?></td>
-      <td><?php echo $res['tipo_usuario'][$i]; ?></td>
-      <td><?php echo $res['correo'][$i]; ?></td>
-      <td><?php echo $res['fecha_reg'][$i]; ?></td>
+      <td class="apt_pat<?= $r ?>"><?php echo $res['apt_pat'][$i]; ?></td>
+      <td class="apt_mat<?= $r ?>"><?php echo $res['apt_mat'][$i]; ?></td>
+      <td class="tipo_usuario<?= $r ?>"><?php echo $res['tipo_usuario'][$i]; ?></td>
+      <td class="correo<?= $r ?>"><?php echo $res['correo'][$i]; ?></td>
+      <td class="fecha_reg<?= $r ?>"><?php echo $res['fecha_reg'][$i]; ?></td>
       <td><?php echo $res['fecha_de'][$i]; ?></td>
       <td class="<?=$res['bg'][$i];?> editar_estatus_<?= $r ?>" data-id="<?=$r?>" data-color="<?=$res['bg'][$i];?>"><?php echo $res['estatusUser'][$i]; ?></td>
       <td>
@@ -37,7 +37,8 @@ foreach($res['id_usuario'] as $i => $r){
           </div> 
           <div class="row editar_acciones_cancelar<?= $r ?>" style="display:none;"> 
               <div class="col">  <i class="bi bi-check2-circle btn save" data-id="<?= $r ?>"></i> </div> 
-              <div class="col"><i class="bi bi-x-circle cancelar btn" data-id="<?=$r?>" data-nombre="<?= $res['nombre'][$i] ?>" data-estatus="<?= $res['estatus'][$i];  ?>" data-color="<?= $res['bg'][$i]; ?>"></i></div>
+              <div class="col"><i class="bi bi-x-circle cancelar btn" data-id="<?=$r?>" data-nombre="<?= $res['nombre'][$i] ?>" data-paterno="<?=$res['apt_pat'][$i] ?>" 
+              data-materno="<?=$res['apt_mat'][$i] ?>" data-tipoUsuario="<?=$res['tipo_usuario'][$i] ?>" data-correo="<?=$res['correo'][$i] ?>" data-estatus="<?= $res['estatus'][$i];  ?>" data-color="<?= $res['bg'][$i]; ?>"></i></div>
               <div class="col"><i class="eliminar bi bi-trash btn" data-id="<?=$r?>"></i></div>
             </div> 
       </td>
